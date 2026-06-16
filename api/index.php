a24dbd3ddd820384b35c719759201533e1e85efe
firaramaita <ramaita91@gmail.com>
2026-06-17 00:01:11 +0700

    fix: alihkan storage laravel ke tmp server vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,14 @@
 <?php
+
+// Mengalihkan folder cache Laravel ke folder temporer Vercel (/tmp)
+$storagePath = '/tmp/storage/framework';
+if (!is_dir($storagePath . '/views')) {
+    mkdir($storagePath . '/views', 0755, true);
+    mkdir($storagePath . '/sessions', 0755, true);
+    mkdir($storagePath . '/cache', 0755, true);
+}
+
+putenv("VIEW_COMPILED_PATH=/tmp/storage/framework/views");
+
+// Memuat bootstraper utama Laravel
 require __DIR__ . '/../public/index.php';
