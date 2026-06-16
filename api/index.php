4457ade144dae3475895b6021a43accf1e6ca9a3
firaramaita <ramaita91@gmail.com>
2026-06-16 20:21:56 +0700

    fix: perbaiki konfigurasi vercel

--- api/index.php
+++ api/index.php
@@ -1,14 +1,2 @@
 <?php
-
-$publicPath = __DIR__ . '/../public';
-
-// Set Laravel public path as root
-chdir($publicPath);
-
-// Set environment variables from Vercel
-if (getenv('VERCEL_ENV') === 'production') {
-    putenv('APP_ENV=production');
-    putenv('APP_DEBUG=false');
-}
-
-require $publicPath . '/index.php';
+require __DIR__ . '/../public/index.php';
