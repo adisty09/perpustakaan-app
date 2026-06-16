248520cf2df053e73b052c4cbcd27784f05596ea
firaramaita <ramaita91@gmail.com>
2026-06-16 18:42:18 +0700

    Deploy ke Vercel

--- api/index.php
+++ api/index.php
@@ -1,7 +1,14 @@
 <?php
 
-/**
- * Here is the serverless function entry
- * for deployment with Vercel.
- */
-require __DIR__.'/../public/index.php';
+$publicPath = __DIR__ . '/../public';
+
+// Set Laravel public path as root
+chdir($publicPath);
+
+// Set environment variables from Vercel
+if (getenv('VERCEL_ENV') === 'production') {
+    putenv('APP_ENV=production');
+    putenv('APP_DEBUG=false');
+}
+
+require $publicPath . '/index.php';
