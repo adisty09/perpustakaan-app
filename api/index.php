a992c89011edd23db29f7ee4d4067baf9284ff67
firaramaita <ramaita91@gmail.com>
2026-06-17 09:11:26 +0700

    test php runtime

--- api/index.php
+++ api/index.php
@@ -1,16 +1,4 @@
 <?php
 
-// Force log to stderr (Vercel compatible)
-putenv("LOG_CHANNEL=stderr");
-putenv("VIEW_COMPILED_PATH=/tmp/storage/framework/views");
-
-// Ensure tmp storage directories exist
-$storagePath = '/tmp/storage/framework';
-$folders = ['views', 'sessions', 'cache'];
-foreach ($folders as $folder) {
-    if (!is_dir($storagePath . '/' . $folder)) {
-        mkdir($storagePath . '/' . $folder, 0755, true);
-    }
-}
-
-require __DIR__ . '/../public/index.php';
+echo "PHP WORKING";
+exit;
